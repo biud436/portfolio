ac85fe17271b47944f97b174bae937e344a9409a
biud436 <biud436@gmail.com>
2021-08-15 17:16:47 +0900

    fixed

--- components/Contact.vue
+++ components/Contact.vue
@@ -2,7 +2,7 @@
   <section class="page-section" id="contact">
     <div class="container">
       <div class="text-center">
-        <h2 class="section-heading text-uppercase">Contact Us</h2>
+        <h2 class="section-heading text-uppercase">Contact ME</h2>
         <h3 class="section-subheading text-muted"></h3>
       </div>
       <form id="contactForm" name="sentMessage" novalidate="novalidate">
