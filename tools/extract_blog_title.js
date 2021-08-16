8609914ca9d5afdf643fdae02e30247c2f6833c6
biud436 <biud436@gmail.com>
2021-08-16 15:39:53 +0900

    added extract_blog_title.js

---
+++ tools/extract_blog_title.js
@@ -0,0 +1,22 @@
+var date = Array.from(
+  document.querySelectorAll(
+    '#listTopForm > table > tbody > tr > td.date > div > span'
+  )
+)
+  .map((e) => e.innerHTML)
+  .reverse();
+var title = Array.from(
+  document.querySelectorAll('.pcol2._setTop._setTopListUrl')
+)
+  .map((e) => {
+    e.textContent = e.textContent.replace(' - RPG Maker MV', '');
+    e.textContent = e.textContent.replace(' - RPG Maker VX Ace', '');
+    e.textContent = e.textContent.replace(' - RPG Maker XP', '');
+  })
+  .reverse();
+
+title
+  .map((e, i, a) => {
+    return `${e} - ${date[i]}`;
+  })
+  .join('\n');
