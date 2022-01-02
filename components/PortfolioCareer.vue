de815691cf76fcc34a66181e2e06a7c249d65e31
biud436 <biud436@gmail.com>
2022-01-02 18:45:30 +0900

    fixed

--- components/PortfolioCareer.vue
+++ components/PortfolioCareer.vue
@@ -29,7 +29,43 @@
             <div class="col-md-12">
               <div class="card-body">
                 <p class="card-text card-description active">
+                  <nuxt-content
+                    :document="contents.dashboard1_2"
+                  ></nuxt-content>
+                </p>
+                <br />
+                <br />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <a id="career-dashboard"></a>
+        <div class="col-md-12 mb-4">
+          <div class="row no-gutters">
+            <div class="col-md-12">
+              <div class="card-body">
+                <p class="card-text card-description active">
                   <nuxt-content :document="contents.dashboard2"></nuxt-content>
+                </p>
+                <br />
+                <br />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <a id="career-dashboard"></a>
+        <div class="col-md-12 mb-4">
+          <div class="row no-gutters">
+            <div class="col-md-12">
+              <div class="card-body">
+                <p class="card-text card-description active">
+                  <nuxt-content
+                    :document="contents.dashboard2_2"
+                  ></nuxt-content>
                 </p>
                 <br />
                 <br />
@@ -49,7 +85,9 @@ export default Vue.extend({
     return {
       contents: {
         dashboard: {},
+        dashboard1_2: {},
         dashboard2: {},
+        dashboard2_2: {},
       },
     };
   },
@@ -59,10 +97,20 @@ export default Vue.extend({
   methods: {
     async loadDashboardData() {
       const markdown1 = await this.$content('career', 'dashboard-01').fetch();
+      const markdown1_2 = await this.$content(
+        'career',
+        'dashboard-01-2'
+      ).fetch();
       const markdown2 = await this.$content('career', 'dashboard-02').fetch();
+      const markdown2_2 = await this.$content(
+        'career',
+        'dashboard-02-2'
+      ).fetch();
 
       this.contents.dashboard = markdown1;
+      this.contents.dashboard1_2 = markdown1_2;
       this.contents.dashboard2 = markdown2;
+      this.contents.dashboard2_2 = markdown2_2;
     },
   },
 });
